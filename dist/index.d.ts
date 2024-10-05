87b6b00a45f9d8c3046d13893b98ba49d910392c
Gungaev Zhamso <zham.drag@gmail.com>
2024-10-05 22:51:17 +0800

    up

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,14 @@
 import { FC } from 'react';
 
+/**
+ * @name Feedback
+ * @description - Компонет UX фидбека
+ *
+ * @param appId - The async callback
+ *
+ * @example
+ * <Feedback appId={number} />
+ */
 declare const Feedback: FC<IFeedBackProps>;
 export default Feedback;
 
--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -2,10 +2,21 @@ import { FC } from "react";
 import { FeedBackProvider } from "../../context/FeedBackContext";
 import { FeedBackModal } from "../FeedBackModal";
 import "../../tailwind.css";
+
+/* Тип IFeedBack */
 interface IFeedBackProps {
+    /* Идентификатор приложения */
     appId: number;
 }
-
+/**
+ * @name Feedback
+ * @description - Компонет UX фидбека
+ *
+ * @param appId - The async callback
+ *
+ * @example
+ * <Feedback appId={number} /> 
+ */
 export const Feedback: FC<IFeedBackProps> = (props) => {
     return (
         <FeedBackProvider>
